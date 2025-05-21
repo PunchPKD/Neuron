754abe342fab6677ea88aad4c5f338446eccc340
PKD <princedangi4281@gmail.com>
2025-05-21 17:42:53 +0530

    slash command implemented with little bit of time control

--- G1/main.py
+++ G1/main.py
@@ -1,10 +1,16 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
+
 import logging
-from dotenv import load_dotenv
 import os
+
+from typing import Literal
+from datetime import datetime, timedelta
+from enum import Enum
+
+from dotenv import load_dotenv
 from AI_Handler import response
-from datetime import datetime
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -21,6 +27,13 @@ bot = commands.Bot(command_prefix='-', intents=intents)
 async def on_ready():
     print(f"done")
 
+    try:
+        await bot.tree.sync()
+        print("synced")
+        
+    except Exception as e:
+        print(f"error : {e}")
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -28,17 +41,21 @@ async def on_message(message):
 
     if "test" in message.content.lower():
         await message.channel.send(f"success1")
-        lastMessage = await message.channel.fetch_message(message.id)
-        await message.channel.send(lastMessage.content)
-    
-    await bot.process_commands(message)
 
-@bot.command()
-async def summary(ctx):
+class duration(Enum):
+    last_one_hour = 1
+    last_two_hour = 2
+    last_three_hour = 3
+
+@bot.tree.command(name="summary", description="summarise the recent chat")
+@app_commands.describe(
+    duration= 'select how much to summarise',
+)
+async def summary(ctx: discord.integrations, duration: duration = 1):
     total = 0
     convoMatrix = []
-    async for messages in ctx.channel.history(limit=30, oldest_first=True, around=datetime.now()):
-        if messages.content != "-count" and messages.author != bot.user:
+    async for messages in ctx.channel.history(oldest_first=True, after=datetime.now()-timedelta(hours=duration.value)):
+        if messages.content != "-summary" and messages.author != bot.user:
             convoMatrix.append([])
             convoMatrix[total].append(messages.author.display_name)
             convoMatrix[total].append(messages.content)
@@ -50,6 +67,6 @@ async def summary(ctx):
         return result
 
     convo = matrix_to_string(convoMatrix)
-    await ctx.message.channel.send(response(convo=convo).text)
+    await ctx.response.send_message(response(convo=convo).text)
 
 bot.run(token=token, log_handler=handler, log_level=logging.DEBUG)
